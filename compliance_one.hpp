67c1a52730f37da1acfecd3b2a0a62aa9018b64b
thibaudk <thibaud.keller@aquilenet.fr>
2023-09-20 22:05:00 +0200

    [cmake] add user class

--- compliance_one.hpp
+++ compliance_one.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
+#include "user.hpp"
 #include "sovereignty.hpp"
 #include "country.hpp"
--- user.hpp
+++ user.hpp
@@ -3,24 +3,25 @@
 
 struct user
 {
-    static const constexpr auto table() { return "user"; };
-    static const constexpr auto primary_key() { return &user::id; };
+    static const constexpr auto table() { return "user"; }
+    static const constexpr auto primary_key() { return &user::id; }
+    static const constexpr auto session_id() { return &user::m_session_id; }
 
     struct id
     {
-        static const constexpr auto c_name() { return "id"; };
+        static const constexpr auto c_name() { return "id"; }
         int32_t value{0};
     } id;
 
     struct username
     {
-        static const constexpr auto c_name() { return "username"; };
+        static const constexpr auto c_name() { return "username"; }
         std::string value{};
     } username;
 
     struct passsword
     {
-        static const constexpr auto c_name() { return "password"; };
+        static const constexpr auto c_name() { return "password"; }
         std::string value{};
     } password;
     
@@ -35,7 +36,13 @@ struct user
 
     struct user_type
     {
-        static const constexpr auto c_name() { return "user_type"; };
+        static const constexpr auto c_name() { return "user_type"; }
         usertypes value{business_user};
     } user_type;
+
+    struct m_session_id
+    {
+        static const constexpr auto c_name() { return "session_id"; }
+        std::string value{};
+    } m_session_id;
 };
