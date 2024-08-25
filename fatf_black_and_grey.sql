24017b662e857a9c3e6da54518a44a59ac0fecc2
thibaudk <thibaud.keller@aquilenet.fr>
2024-08-25 21:16:15 +0100

    [sql] add fatf scores

---
+++ fatf_black_and_grey.sql
@@ -0,0 +1,28 @@
+-- taken from https://www.fatf-gafi.org/en/countries/black-and-grey-lists.html
+-- Change "Democratic People's Republic of Korea" to "North Korea"
+-- Change ""Democratic Republic of Congo" to "Democratic Republic of the Congo"
+
+UPDATE country SET fatf = 0 WHERE name = "North Korea";
+UPDATE country SET fatf = 0 WHERE name = "Iran";
+UPDATE country SET fatf = 0 WHERE name = "Myanmar";
+UPDATE country SET fatf = 0 WHERE name = "Bulgaria";
+UPDATE country SET fatf = 0 WHERE name = "Burkina Faso";
+UPDATE country SET fatf = 0 WHERE name = "Cameroon";
+UPDATE country SET fatf = 0 WHERE name = "Croatia";
+UPDATE country SET fatf = 0 WHERE name = "Democratic Republic of the Congo";
+UPDATE country SET fatf = 0 WHERE name = "Haiti";
+UPDATE country SET fatf = 0 WHERE name = "Kenya";
+UPDATE country SET fatf = 0 WHERE name = "Mali";
+UPDATE country SET fatf = 0 WHERE name = "Monaco";
+UPDATE country SET fatf = 0 WHERE name = "Mozambique";
+UPDATE country SET fatf = 0 WHERE name = "Namibia";
+UPDATE country SET fatf = 0 WHERE name = "Nigeria";
+UPDATE country SET fatf = 0 WHERE name = "Philippines";
+UPDATE country SET fatf = 0 WHERE name = "Senegal";
+UPDATE country SET fatf = 0 WHERE name = "South Africa";
+UPDATE country SET fatf = 0 WHERE name = "South Sudan";
+UPDATE country SET fatf = 0 WHERE name = "Syria";
+UPDATE country SET fatf = 0 WHERE name = "Tanzania";
+UPDATE country SET fatf = 0 WHERE name = "Venezuela";
+UPDATE country SET fatf = 0 WHERE name = "Vietnam";
+UPDATE country SET fatf = 0 WHERE name = "Yemen";
---
+++ fatf_members.sql
@@ -0,0 +1,44 @@
+-- taken from the "FATF Member Countries" tab in "countries" drop down at https://www.fatf-gafi.org/content/fatf-gafi/en/publications/Mutualevaluations/Assessment-ratings.html
+-- Change "Hong Kong, China" to "Hong Kong S.A.R."
+-- Change "Korea" to "South Korea"
+-- Change "Türkiye" to "Turkey"
+
+UPDATE country SET fatf = 5;
+UPDATE country SET fatf = 10 WHERE name = "Argentina";
+UPDATE country SET fatf = 10 WHERE name = "Australia";
+UPDATE country SET fatf = 10 WHERE name = "Austria";
+UPDATE country SET fatf = 10 WHERE name = "Belgium";
+UPDATE country SET fatf = 10 WHERE name = "Brazil";
+UPDATE country SET fatf = 10 WHERE name = "Canada";
+UPDATE country SET fatf = 10 WHERE name = "China";
+UPDATE country SET fatf = 10 WHERE name = "Denmark";
+UPDATE country SET fatf = 10 WHERE name = "Finland";
+UPDATE country SET fatf = 10 WHERE name = "France";
+UPDATE country SET fatf = 10 WHERE name = "Germany";
+UPDATE country SET fatf = 10 WHERE name = "Greece";
+UPDATE country SET fatf = 10 WHERE name = "Hong Kong S.A.R.";
+UPDATE country SET fatf = 10 WHERE name = "Iceland";
+UPDATE country SET fatf = 10 WHERE name = "India";
+UPDATE country SET fatf = 10 WHERE name = "Indonesia";
+UPDATE country SET fatf = 10 WHERE name = "Ireland";
+UPDATE country SET fatf = 10 WHERE name = "Israel";
+UPDATE country SET fatf = 10 WHERE name = "Italy";
+UPDATE country SET fatf = 10 WHERE name = "Japan";
+UPDATE country SET fatf = 10 WHERE name = "South Korea";
+UPDATE country SET fatf = 10 WHERE name = "Luxembourg";
+UPDATE country SET fatf = 10 WHERE name = "Malaysia";
+UPDATE country SET fatf = 10 WHERE name = "Mexico";
+UPDATE country SET fatf = 10 WHERE name = "Netherlands";
+UPDATE country SET fatf = 10 WHERE name = "New Zealand";
+UPDATE country SET fatf = 10 WHERE name = "Norway";
+UPDATE country SET fatf = 10 WHERE name = "Portugal";
+UPDATE country SET fatf = 10 WHERE name = "Saudi Arabia";
+UPDATE country SET fatf = 10 WHERE name = "Singapore";
+UPDATE country SET fatf = 10 WHERE name = "South Africa";
+UPDATE country SET fatf = 10 WHERE name = "Spain";
+UPDATE country SET fatf = 10 WHERE name = "Sweden";
+UPDATE country SET fatf = 10 WHERE name = "Switzerland";
+UPDATE country SET fatf = 10 WHERE name = "Turkey";
+UPDATE country SET fatf = 10 WHERE name = "United Kingdom";
+UPDATE country SET fatf = 10 WHERE name = "United States";
+
